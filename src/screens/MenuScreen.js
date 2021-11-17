0d9562e973cc101398ac0b1b5e68b23cd4b1e945
trungkien263 <trungkienvg263@gmail.com>
2021-11-17 19:19:01 +0700

    update skeleton UI

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,43 +1,22 @@
 import React from 'react';
-import {Dimensions, View} from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 const windowWidth = Dimensions.get('window').width;
 
 export default function MenuScreen() {
   return (
-    <SkeletonPlaceholder>
-      <View
-        style={{
-          flexDirection: 'column',
-          padding: 16,
-        }}>
-        {/* Card Item */}
-        <View style={{paddingHorizontal: 16}}>
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              marginBottom: 10,
-            }}>
-            <View style={{width: 60, height: 60, borderRadius: 50}} />
-            <View style={{marginLeft: 20}}>
-              <View style={{width: 200, height: 20, borderRadius: 4}} />
-              <View
-                style={{marginTop: 6, width: 80, height: 20, borderRadius: 4}}
-              />
-            </View>
-          </View>
-          <View
-            style={{width: windowWidth - 64, height: 350, borderRadius: 10}}
-          />
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <View style={{width: 60, height: 20}} />
-            <View style={{width: 60, height: 20}} />
-            <View style={{width: 60, height: 20}} />
-          </View>
-        </View>
-        {/* Card Item */}
+    <View style={styles.container}>
+      <View style={{flex: 1}}>
+        <Text>KKK</Text>
       </View>
-    </SkeletonPlaceholder>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    alignItems: 'center',
+  },
+});
