c5555b3e5079cf7265580a04268cb11a3c5814e8
trungkien263 <trungkienvg263@gmail.com>
2021-11-24 23:16:57 +0700

    setup menu screen UI

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,7 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  FlatList,
+} from 'react-native';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 import callApi from '../services/apiCaller';
+import LinearGradient from 'react-native-linear-gradient';
 
 const windowWidth = Dimensions.get('window').width;
 
@@ -15,30 +23,93 @@ export default function MenuScreen() {
 
   const lastItem = covidCases.pop();
 
-  console.log('======cases', lastItem);
+  //   console.log('======cases', lastItem);
+
+  const renderData = [
+    {
+      title: 'Chế độ ăn uống',
+      action: () => {
+        alert('Diet Clicked !');
+      },
+    },
+    {
+      title: 'SOS',
+      action: () => {
+        alert('SOS Clicked !');
+      },
+    },
+    {
+      title: 'Chế độ ăn uống',
+      action: () => {
+        alert('Clicked !');
+      },
+    },
+    {
+      title: 'Chế độ ăn uống',
+      action: () => {
+        alert('Clicked !');
+      },
+    },
+  ];
+
+  const covidData = [
+    {
+      title: 'Total Confirmed',
+      data: lastItem?.Confirmed,
+    },
+    {
+      title: 'Total Recovered',
+      data: lastItem?.Recovered,
+    },
+    {
+      title: 'Total Deaths',
+      data: lastItem?.Deaths,
+    },
+  ];
+
+  const MenuItem = ({title, action}) => {
+    return (
+      <TouchableOpacity onPress={action} style={{marginTop: 16}}>
+        <LinearGradient
+          style={{
+            height: 100,
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          colors={['#4c669f', '#3b5998', '#192f6a']}>
+          <Text style={styles.title}>{title}</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+    );
+  };
+
+  const CovidItem = ({title, data}) => {
+    return (
+      <LinearGradient
+        colors={['#4c669f', '#3b5998', '#192f6a']}
+        style={styles.box}>
+        <Text style={[styles.title, {fontSize: 14}]}>{title}</Text>
+        <Text style={[styles.title, {fontSize: 14}]}>{data}</Text>
+      </LinearGradient>
+    );
+  };
 
   return (
     <View style={styles.container}>
       <View
         style={{
-          flex: 1,
           flexDirection: 'row',
           justifyContent: 'space-between',
           width: windowWidth - 32,
         }}>
-        <View style={styles.box}>
-          <Text>Total Confirmed</Text>
-          <Text>{lastItem.Confirmed}</Text>
-        </View>
-        <View style={styles.box}>
-          <Text>Total Recovered</Text>
-          <Text>{lastItem.Recovered}</Text>
-        </View>
-        <View style={styles.box}>
-          <Text>Total Deaths</Text>
-          <Text>{lastItem.Deaths}</Text>
-        </View>
+        {covidData.map((el, i) => {
+          return <CovidItem title={el.title} data={el.data} />;
+        })}
       </View>
+      {renderData.map((el, i) => {
+        return <MenuItem key={i} title={el.title} action={el.action} />;
+      })}
     </View>
   );
 }
@@ -47,14 +118,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    alignItems: 'center',
   },
   box: {
     backgroundColor: 'orange',
     paddingVertical: 8,
-    height: 56,
+    height: 100,
     alignItems: 'center',
     justifyContent: 'center',
     width: (windowWidth - 52) / 3,
+    borderRadius: 10,
+  },
+  title: {
+    color: '#fff',
+    fontWeight: '700',
+    fontSize: 16,
   },
 });
