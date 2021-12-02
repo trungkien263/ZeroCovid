c36677910883850b6c6b4f5e3480477417a6e5c5
trungkien263 <trungkienvg263@gmail.com>
2021-12-02 23:56:05 +0700

    add address selection

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -6,11 +6,16 @@ import {
   TouchableOpacity,
   View,
   FlatList,
+  ScrollView,
+  Picker,
 } from 'react-native';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
+import SearchableDropdown from 'react-native-searchable-dropdown';
+
 import callApi from '../services/apiCaller';
 import LinearGradient from 'react-native-linear-gradient';
 import {useSelector} from 'react-redux';
+import provinces from '../data/provinces.json';
 
 const windowWidth = Dimensions.get('window').width;
 
@@ -20,10 +25,41 @@ export default function MenuScreen() {
   const vnNumber = vnCases.pop();
   const [covidCases, setCovidCases] = useState(vnNumber);
   const [isTotalWorld, setIsTotalWorld] = useState(false);
+  const [provinceData, setProvinceData] = useState([]);
+  const [districtList, setDistrictList] = useState([]);
+  const [wardList, setWardList] = useState([]);
+  const [selectedProvince, setSelectedProvince] = useState('01');
+  const [selectedDistrict, setSelectedDistrict] = useState([]);
+  const [selectedWard, setSelectedWard] = useState([]);
 
-  console.log('----------useDetails', useDetails);
+  useEffect(() => {
+    const arr = [];
+    provinces.map(el => {
+      arr.push({
+        id: el.Id,
+        name: el.Name,
+        district: el.Districts,
+      });
+    });
+    setProvinceData(arr);
+  }, []);
 
-  //   const lastItem = vnCases.pop();
+  useEffect(async () => {
+    if (selectedProvince.length > 0) {
+      const province = provinces.find(el => el.Id === selectedProvince);
+      await setDistrictList(province.Districts);
+      console.log('++++++districtList[0]:', districtList[0].Wards);
+      const district = districtList[0].Wards;
+      setWardList(district);
+    }
+  }, [selectedProvince]);
+
+  useEffect(() => {
+    if (districtList.length > 0) {
+      const district = districtList.find(el => el.Id === selectedDistrict);
+      setWardList(district.Wards);
+    }
+  }, [selectedDistrict]);
 
   const renderData = [
     {
@@ -36,18 +72,6 @@ export default function MenuScreen() {
       title: 'SOS',
       action: () => {
         alert('SOS Clicked !');
-      },
-    },
-    {
-      title: 'Chế độ ăn uống',
-      action: () => {
-        alert('Clicked !');
-      },
-    },
-    {
-      title: 'Chế độ ăn uống',
-      action: () => {
-        alert('Clicked !');
       },
     },
   ];
@@ -99,8 +123,17 @@ export default function MenuScreen() {
     );
   };
 
+  const ProvinceItem = (label, value) => {
+    return (
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Text>{label}</Text>
+        <Text>{value}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View
         style={{
           flexDirection: 'row',
@@ -157,7 +190,63 @@ export default function MenuScreen() {
       {renderData.map((el, i) => {
         return <MenuItem key={i} title={el.title} action={el.action} />;
       })}
-    </View>
+
+      <View style={{marginTop: 16}}>
+        <View style={{borderWidth: 2, borderColor: '#ccc', borderRadius: 10}}>
+          <Picker
+            selectedValue={selectedProvince}
+            style={{height: 50}}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedProvince(itemValue)
+            }>
+            {provinces.map((el, i) => (
+              <ProvinceItem key={i} label={el.Name} value={el.Id} />
+            ))}
+          </Picker>
+        </View>
+        <View
+          style={{
+            borderWidth: 2,
+            borderColor: '#ccc',
+            borderRadius: 10,
+            marginTop: 10,
+          }}>
+          <Picker
+            selectedValue={selectedDistrict}
+            style={{height: 50}}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedDistrict(itemValue)
+            }>
+            {districtList.map((el, i) => (
+              <ProvinceItem key={i} label={el.Name} value={el.Id} />
+            ))}
+          </Picker>
+        </View>
+        <View
+          style={{
+            borderWidth: 2,
+            borderColor: '#ccc',
+            borderRadius: 10,
+            marginTop: 10,
+          }}>
+          <Picker
+            selectedValue={selectedWard}
+            style={{height: 50}}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedWard(itemValue)
+            }>
+            {wardList.map((el, i) => (
+              <ProvinceItem key={i} label={el.Name} value={el.Id} />
+            ))}
+          </Picker>
+        </View>
+        <View style={{marginTop: 10}}>
+          <Text>
+            {selectedProvince + ' ' + selectedDistrict + ' ' + selectedWard}
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
